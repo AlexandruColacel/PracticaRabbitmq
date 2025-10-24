801e3555573c53ede62323e9daed723b5a51b5ed
FERNANDO GARCÍA NAVARRO <Fernando.Garcia24@alu.uclm.es>
2025-10-24 09:15:39 +0200

    DTO para seleccionar en el caso de uso de Reseñas

---
+++ src/AppForSEII2526.API/DTOs/DeviceDTO/DeviceParaReseñasDTO.cs
@@ -0,0 +1,32 @@
+﻿namespace AppForSEII2526.API.DTOs.DeviceDTO
+{
+    public class DeviceParaReseñasDTO
+    {
+        public DeviceParaReseñasDTO(int id, string name, string brand, string color, int year, string model)
+        {
+            Id = id;
+            Name = name;
+            Brand = brand;
+            Color = color;
+            Year = year;
+            Model = model;
+        }
+
+
+        //la lista de dispositivos, indicando el nombre, la marca, el color, el año, y el modelo.
+        public int Id { get; set; }
+
+        public string Name { get; set; }    
+
+        public string Brand { get; set; }
+
+        public string Color { get; set; }
+
+        public int Year { get; set; }
+        
+        public string Model { get; set; }
+
+
+
+    }
+}
