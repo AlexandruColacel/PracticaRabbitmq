ea8dd948836924f2dc9a99f96db82f15151179bb
ALEXANDRU COLACEL <Alexandru.Colacel@alu.uclm.es>
2025-11-03 16:02:26 +0100

    Creacion metodo POST

--- src/AppForSEII2526.API/DTOs/RentalDTOs/RentalItemDTO.cs
+++ src/AppForSEII2526.API/DTOs/RentalDTOs/RentalItemDTO.cs
@@ -2,15 +2,19 @@
 {
     public class RentalItemDTO
     {
-        public RentalItemDTO(string model, string brand, double rentPrice)
+        public RentalItemDTO(int id,string model, string brand, double rentPrice, int quantity)
         {
+            Id = id;
             Model = model;
             Brand = brand;
             RentPrice = rentPrice;
+            Quantity = quantity;
         }
 
+        public int Id { get; set; }
         public string Model { get; set; }
         public string Brand { get; set; }
         public double RentPrice { get; set; }
+        public int Quantity { get; set; }
     }
 }
